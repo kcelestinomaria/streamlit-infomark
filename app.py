b89851b3e9ac444200d2dab14a6c62cfd5fc5164
Celestine Kariuki <celestino@safariblocks.com>
2024-08-08 18:21:54 +0000

    bnow

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st 
+from openbb import obb 
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+personal_access_token = os.getenv('PAT')
 
 st.title('My Streamlit App')
 st.write('Hello, Streamlit!')
+
+obb.account.login(pat=personal_access_token)
